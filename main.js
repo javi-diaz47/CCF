5e1a29e632be25c686d41987cd9253390bdd0db4
Javier Diaz <javiereduardo300@gmail.com>
2021-06-21 16:58:23 -0500

    added the counter configuration into the main.js

---
+++ main.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Hello World!");
+
+    let currentNumber = 0;
+
+    // COUNTER
+    const counterNumber = document.querySelector('#counter-number');
+
+    // COUNTER BUTTONS
+    const resetBtn = document.querySelector('#reset-btn');
+    resetBtn.onclick = () => resetCounter();
+    
+    const increaseBtn = document.querySelector('#increase-btn');
+    increaseBtn.onclick = () => increaseCounter();
+    
+    const decreaseBtn = document.querySelector('#decrease-btn');
+    decreaseBtn.onclick = () => decreaseCounter();
+
+    
+    //Increase counter
+    const increaseCounter = () => {
+        currentNumber++;
+        setCounterNumber(currentNumber);
+        console.log('increased');
+    }
+
+    //Reset counter 
+    const resetCounter = () => {
+        currentNumber = 0;
+        setCounterNumber(currentNumber);
+    }
+
+   //Decrease counter
+    const decreaseCounter = () => {
+        currentNumber--;
+        setCounterNumber(currentNumber);
+    }
+
+
+
+    // Set the current number in the DOM
+    const setCounterNumber = (number) => {
+        setCounterNumberColor(number);
+        counterNumber.textContent = number;
+    }
+
+    const setCounterNumberColor = (number) => {
+
+        if(number > 0){
+            counterNumber.classList = 'positive active-number counter-number';
+        }else if(number == 0){
+            counterNumber.classList = 'counter-number'
+        }else{
+            counterNumber.classList = 'negative active-number counter-number';
+        }
+
+    }
+    
+});
